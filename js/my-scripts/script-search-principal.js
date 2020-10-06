66a8d7c494a2a85675123313244a5c4b1ecf14d0
Roberto Ramirez <42901082+robertpiscis2@users.noreply.github.com>
2020-10-06 18:01:47 -0500

    Update script-search-principal.js

--- js/my-scripts/script-search-principal.js
+++ js/my-scripts/script-search-principal.js
@@ -84,34 +84,7 @@ async function searcherGet(texto, country, city, titleText){
             <h5 class="card-title">${it.nombreProducto}</h5>
             <p class="card-text" style="height: 80px;">${it.descripcion}</p>
             <p class="card-text ${colorState} state-text">${it.estado}</p>
-            <div class="d-flex flex-row justify-content-center mb-3">
-                <button href="#modalProduct${cont}" ${statusProduct} class="btn btn-primary btn-bkg" data-toggle="modal" onclick="updateViews(${it.visitas})">Truequear</button>
-                <!--START modalProduct-->
-                <div class="modal fade" tabindex="-1" role="dialog" id="modalProduct${cont}">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title">${it.nombreProducto}</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body d-flex flex-column align-items-center">
-                            <img src="${urlFotos}${it.fotos[0]}" alt="" class="mb-3" width=300 height=300>
-                            <p>${it.descripcion}</p>
-                        </div>
-                        <div class="modal-footer">
-                            <!-- <div><h3 class="text-primary">Contactar</h3></div> -->
-                            <div class="anchor d-flex flex-row justify-content-center">
-                                <a href="mailto:${it.email}" class="btn btn-info mr-1 btn-contact"><i class="far fa-envelope"></i> Email</a>
-                                <a href="https://wa.me/51${it.celular}?text=Me%20gustar%C3%ADa%20obtener%20tu%20servicio%20o%20producto" target="_blank" class="btn btn-success ml-1 btn-contact"><i class="fab fa-whatsapp"></i> Whatsapp</a>        
-                            </div>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-                <!--modalProduct-->
-            </div>
+            <img src="${urlFotos}${it.fotos[0]}" alt="" class="mb-3" width=100 height=100>
         </div>
     </article>`)
     }
